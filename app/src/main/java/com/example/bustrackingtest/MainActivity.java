fdcfd4943955f1b1a92024129242a163c454bad8
not-ho1d <navaneeth06.16@gmail.com>
2026-01-10 19:24:13 +0000

    basic ui

--- app/src/main/java/com/example/bustrackingtest/MainActivity.java
+++ app/src/main/java/com/example/bustrackingtest/MainActivity.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView view_in_map_button = findViewById(R.id.view_map_button);
+        view_in_map_button.setOnClickListener(v->{
+            setContentView(R.layout.activity_view_map);
+        });
+
 
         AutoCompleteTextView fromStand = findViewById(R.id.fromStand);
         AutoCompleteTextView toStand = findViewById(R.id.toStand);
