95365980ffe75a5c400075c61e9b3acf9f0f29d9
not-ho1d <navaneeth06.16@gmail.com>
2026-01-10 18:02:50 +0000

    basic ui

--- app/src/main/java/com/example/bustrackingtest/MainActivity.java
+++ app/src/main/java/com/example/bustrackingtest/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.bustrackingtest;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView fromStand = findViewById(R.id.fromStand);
+        AutoCompleteTextView toStand = findViewById(R.id.toStand);
+
+        String[] stands = {"dwaraka","mananthavady","4th-mile","thonichal","nadakkal","tharuvana"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.dropdown,stands);
+        fromStand.setAdapter(adapter);
+        toStand.setAdapter(adapter);
     }
 }
