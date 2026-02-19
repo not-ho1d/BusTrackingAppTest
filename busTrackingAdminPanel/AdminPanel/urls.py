a72d88808bc0e4cc00f26649fcd5fff55b9e8856
HOID <navaneeth06.16@gmail.com>
2026-02-20 00:41:31 +0530

    wrong repo- shi

--- busTrackingAdminPanel/AdminPanel/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path("add_routes/",views.AddRoutes,name="add_routes"),
-    path("edit_stops/",views.EditStops,name="edit_stops")
-
-]
